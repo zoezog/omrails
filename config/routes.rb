1422b0462568f2085c71445bc6e6b7a06c1e35f9
Zoe Zog <oliver.gayagoy@gmail.com>
2016-10-17 16:21:58 -0400

    Add content to home, about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'pages/home'
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contact-us' => 'pages#contactus'
+  get 'contactus' => 'pages#contactus'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
