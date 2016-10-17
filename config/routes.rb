cbd699b36c8f1fc63d57a0417295a42d80735382
Zoe Zog <oliver.gayagoy@gmail.com>
2016-10-17 14:52:19 -0400

    Added pages, home, about Us and Contact Us

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact-us' => 'pages#contactus'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
